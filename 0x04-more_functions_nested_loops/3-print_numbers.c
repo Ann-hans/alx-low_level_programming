00e619cd1c77e94dfe80bfd09a810830650cc947
Ann-hans <ahanson225.ah@gmail.com>
2022-10-20 16:32:59 -0700

    c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * print_numbers - print 0123456789
- * owned by Ann-hans
+ * *print_numbers - print 0123456789
  * Return: void
  */
 void print_numbers(void)
@@ -9,7 +8,7 @@ void print_numbers(void)
 char c;
 for (c = '0'; c <= '9'; c++)
 {
-_putchar(c);
+_utchar(c);
 }
 _putchar('\n');
 }
